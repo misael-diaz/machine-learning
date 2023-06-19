f3d346a5c4b55ef4eea9dc5b1967c7b0127132e9
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2023-06-19 14:43:36 -0400

    removes old KNN test [cxx]

--- kthNearestNeighbor/cxx/main.cpp
+++ kthNearestNeighbor/cxx/main.cpp
@@ -46,12 +46,10 @@ struct Data
   }
 };
 
-//void test_knn();
 void test();
 
 int main ()
 {
-  //test_knn();
   test();
   return 0;
 }
@@ -253,124 +251,6 @@ void test ()
   }
 
   std::cout << "knn-test: ";
-  if (diff != 0)
-  {
-    std::cout << "FAIL" << std::endl;
-  }
-  else
-  {
-    std::cout << "PASS" << std::endl;
-  }
-
-}
-
-
-void test_knn ()
-{
-  // we need this predicate lambda function for sorting the dataset
-  auto const pred = [](const Data& data1, const Data& data2) -> bool {
-    return (data1.X < data2.X);
-  };
-
-  std::vector<Data> dset = dataset();
-  std::sort(dset.begin(), dset.end(), pred);
-
-  Data const target(128, 0);
-  auto const div = std::lower_bound(dset.begin(), dset.end(), target, pred);
-  int index = std::distance(dset.begin(), div);
-
-  // stores the nearest neighbors for testing the implementation of the KNN algorithm
-  std::vector<double> distances;
-  for (auto& elem : dset)
-  {
-    double const dist = elem.dist(target);
-    distances.push_back(dist);
-  }
-
-  std::sort(distances.begin(), distances.end());
-
-  // finds the 1st nearest neighbor
-  int i = (index - 1);
-  int j = index;
-  Data first(dset[i]);
-  Data second(dset[j]);
-  double d1 = first.dist(target);
-  double d2 = second.dist(target);
-
-  if (d2 < d1)
-  {
-    int larger = i;
-    i = j;
-    j = larger;
-
-    double const d = d1;
-    Data tmp(first);
-
-    first = second;
-    second = tmp;
-    
-    d1 = d2;
-    d2 = d;
-  }
-
-  // gets the index of the next tree node
-  index = (i < j)? (i - 1) : (i + 1);
-
-  // initializes the error of the kth nearest neighbor
-  double diff = (distances[0] - d1);
-
-  int last = 0;
-  // finds the 2nd, 3rd, 4th, etc. nearest neighbors dynamically by traversing the tree
-  for (int k = 1; k != 200; ++k)
-  {
-    Data data(dset[index]);
-    double d = data.dist(target);
-
-    if (d < d2)
-    {
-      Data tmp(second);
-      second = data;
-      data = tmp;
-
-      int larger = j;
-      j = index;
-      index = larger;
-    }
-
-    i = j;
-    j = index;
-
-    first = second;
-    second = data;
-
-    d1 = first.dist(target);
-    d2 = second.dist(target);
-
-    index = (i < j)? (i - 1) : (i + 1);
-
-    // updates the error
-    diff += (distances[k] - d1);
-
-    // switches algorithm if the left partition is depleted:
-    if (index < 0)
-    {
-      last = (k + 1);
-      index = j;
-      break;
-    }
-    
-  }
-
-  // if the left partition is depleted we continue with the right partition in order
-  for (int k = last; k != 200; ++k)
-  {
-    Data data(dset[index]);
-    double d = data.dist(target);
-    diff += (distances[k] - d);
-    ++index;
-  }
-
-  std::cout << "test[0]: ";
   if (diff != 0)
   {
     std::cout << "FAIL" << std::endl;
