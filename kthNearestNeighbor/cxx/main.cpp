78154b75bd7e49b2aa303dce0237aabfce88aa56
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2023-06-19 14:35:10 -0400

    fixes implementation of the KNN() algorithm [cxx]
    COMMENTS:
    former KNN() failed sometimes for other targets if one of the
    partitions was depleted during traversal
    
    fixes unreported memory issues of the former KNN() implementation
    
    improves KNN() by accounting for targets whose value is smaller
    (larger) than the minimum (maximum) value of the dataset.
    
    KNN() passes test successfully for all considered values. We look
    for all the possible Kth Nearest Neighbors for each target value;
    the targets are all the elements of the dataset.
    
    valgrind reports no memorry issues for this KNN() version

--- kthNearestNeighbor/cxx/main.cpp
+++ kthNearestNeighbor/cxx/main.cpp
@@ -38,7 +38,7 @@ struct Data
 
   Data(double const X, double const y) : X(X), y(y) {}
 
-  double dist(const Data& data)
+  double dist(const Data& data) const
   {
     double const x1 = this -> X;
     double const x2 = data.X;
@@ -46,11 +46,13 @@ struct Data
   }
 };
 
-void test_knn();
+//void test_knn();
+void test();
 
 int main ()
 {
-  test_knn();
+  //test_knn();
+  test();
   return 0;
 }
 
@@ -81,6 +83,187 @@ std::vector<Data> dataset ()
 
   return dset;
 }
+
+
+Data knn (int const Kth, Data const& target, std::vector<Data>& dset)
+{
+  // we need this predicate lambda function for sorting the dataset
+  auto const pred = [](const Data& data1, const Data& data2) -> bool {
+    return (data1.X < data2.X);
+  };
+
+  std::sort(dset.begin(), dset.end(), pred);
+
+  // divides into left and right partitions
+  auto const div = std::lower_bound(dset.begin(), dset.end(), target, pred);
+  int index = std::distance(dset.begin(), div);
+
+  if (index == 0)
+  {
+    // target is greater than or equal to min value in dataset, O(1) look up in rigth
+    return dset[Kth - 1];
+  }
+
+  int size = dset.size();
+  if ( index == (size - 1) )
+  {
+    // target is less than or equal to max value in dataset, O(1) loop up in left
+    return dset[size - Kth];
+  }
+
+  // traversal algorithm: finds the 1st nearest neighbor
+  int i = (index - 1);
+  int j = index;
+  Data first(dset[i]);
+  Data second(dset[j]);
+  double d1 = first.dist(target);
+  double d2 = second.dist(target);
+
+  if (d2 < d1)
+  {
+    int larger = i;
+    i = j;
+    j = larger;
+
+    double const d = d1;
+    Data tmp(first);
+
+    first = second;
+    second = tmp;
+
+    d1 = d2;
+    d2 = d;
+  }
+
+  index = (i < j)? (i - 1) : (i + 1);
+
+  if (Kth == 1)
+  {
+    return first;
+  }
+
+  // traversal algorithm: finds the 2nd, 3rd, 4th, etc. nearest neighbors dynamically
+
+  int last = 1;
+  bool forward = true;
+  for (int k = 1; k != Kth; ++k)
+  {
+    Data data(dset[index]);
+    double d = data.dist(target);
+
+    if (d < d2)
+    {
+      Data tmp(second);
+      second = data;
+      data = tmp;
+
+      int larger = j;
+      j = index;
+      index = larger;
+    }
+
+    i = j;
+    j = index;
+
+    first = second;
+    second = data;
+
+    d1 = first.dist(target);
+    d2 = second.dist(target);
+
+    index = (i < j)? (i - 1) : (i + 1);
+
+    // switches algorithm if the left partition is depleted:
+    if (index < 0)
+    {
+      forward = true;
+      last = (k + 1);
+      index = j;
+      break;
+    }
+
+    // switches algorithm if the right partition is depleted:
+    if (index >= size)
+    {
+      forward = false;
+      last = (k + 1);
+      index = j;
+      break;
+    }
+
+  }
+
+  // returns the Kth nearest neighbor when the traversal algorithm succeeds:
+  if (last == 1 || last == Kth)
+  {
+    // Note:
+    // when last == Kth, this means the algorithm succeeded just in time since there were
+    // no more elements in the left (or right) partition when the algorithm succeeded
+    return first;
+  }
+
+
+  // gets the Kth nearest neighbor in the right partition, O(1), by computing its location
+  if (forward)
+  {
+    index += (Kth - last - 1);
+    first = dset[index];
+    return first;
+  }
+  else
+  {
+    index -= (Kth - last - 1);
+    first = dset[index];
+    return first;
+  }
+}
+
+
+// tests by looking all the kth nearest neighbors for each dataset element (target)
+void test ()
+{
+  // we need this predicate lambda function for sorting the dataset
+  auto const pred = [](const Data& data1, const Data& data2) -> bool {
+    return (data1.X < data2.X);
+  };
+
+  std::vector<Data> dset = dataset();
+  std::sort(dset.begin(), dset.end(), pred);
+
+  double diff = 0;
+  for (const auto& target : dset)
+  {
+    std::vector<double> distances;
+    for (const auto& elem : dset)
+    {
+      double const dist = elem.dist(target);
+      distances.push_back(dist);
+    }
+
+    std::sort(distances.begin(), distances.end());
+
+    for (std::vector<Data>::size_type k = 0; k != dset.size(); ++k)
+    {
+      int const Kth = (k + 1);
+      Data const kthElem = knn(Kth, target, dset);
+      double const computed = kthElem.dist(target);
+      double const expected = distances[k];
+      diff += (computed - expected) * (computed - expected);
+    }
+  }
+
+  std::cout << "knn-test: ";
+  if (diff != 0)
+  {
+    std::cout << "FAIL" << std::endl;
+  }
+  else
+  {
+    std::cout << "PASS" << std::endl;
+  }
+
+}
+
 
 void test_knn ()
 {
